cbc02f3d0f685b200f91672456320e3da7233e05
Billa Saikumar <BS00779804@techmahindra.com>
2024-06-26 13:42:52 +0530

    using useReducer react hook

--- src/Cart/Store/CartContext.jsx
+++ src/Cart/Store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useReducer } from "react";
 import { ProductsList } from "../ProductsList";
 
 export const CartContext = createContext({
@@ -7,49 +7,65 @@ export const CartContext = createContext({
   handleAddToCart: () => {},
 });
 
+function ShoppingCartReducer(state, actions) {
+  if (actions.type == "Add_Item") {
+    const UpdatedItems = [...state];
+    const existingItemIndex = UpdatedItems.findIndex(
+      (item) => item.id === actions.Id
+    );
+    if (UpdatedItems[existingItemIndex]) {
+      const UpdateItem = UpdatedItems[existingItemIndex];
+      UpdateItem.count = UpdateItem.count + 1;
+      UpdatedItems[existingItemIndex] = UpdateItem;
+    } else {
+      const NewItem = ProductsList.find((item) => item.id === actions.Id);
+      UpdatedItems.push(NewItem);
+    }
+    return UpdatedItems;
+  } else if (actions.type == "Update_Item") {
+    const UpdateItems = [...state];
+    const existingItemIndex = UpdateItems.findIndex(
+      (item) => item.id === actions.Id
+    );
+    const UpdateItem = UpdateItems[existingItemIndex];
+    if (actions.op == "+") {
+      UpdateItem.count++;
+    } else if (actions.op == "-") {
+      console.log("count", UpdateItem.count);
+      UpdateItem.count--;
+      if (UpdateItem.count <= 0) {
+        UpdateItems.pop(existingItemIndex);
+        return UpdateItems;
+      }
+    }
+    UpdateItems[existingItemIndex] = UpdateItem;
+    return UpdateItems;
+  }
+}
+
 export function CartContextLogic({ children }) {
-  const [cartlist, setCartList] = useState([]);
+  const [shopingCartState, shoppingCartDispatch] = useReducer(
+    ShoppingCartReducer,
+    []
+  );
 
   function handleAddToCart(id) {
-    setCartList((prevItmes) => {
-      const UpdatedItems = [...prevItmes];
-      const existingItemIndex = UpdatedItems.findIndex(
-        (item) => item.id === id
-      );
-      if (UpdatedItems[existingItemIndex]) {
-        const UpdateItem = UpdatedItems[existingItemIndex];
-        UpdateItem.count = UpdateItem.count + 1;
-        UpdatedItems[existingItemIndex] = UpdateItem;
-      } else {
-        const NewItem = ProductsList.find((item) => item.id === id);
-        UpdatedItems.push(NewItem);
-      }
-      return UpdatedItems;
+    shoppingCartDispatch({
+      type: "Add_Item",
+      Id: id,
     });
   }
 
   function handleCountChane(op, id) {
-    setCartList((prevItems) => {
-      const UpdateItems = [...prevItems];
-      const existingItemIndex = UpdateItems.findIndex((item) => item.id === id);
-      const UpdateItem = UpdateItems[existingItemIndex];
-      if (op == "+") {
-        UpdateItem.count++;
-      } else if (op == "-") {
-        console.log("count", UpdateItem.count);
-        UpdateItem.count--;
-        if (UpdateItem.count <= 0) {
-          UpdateItems.pop(existingItemIndex);
-          return UpdateItems;
-        }
-      }
-      UpdateItems[existingItemIndex] = UpdateItem;
-      return UpdateItems;
+    shoppingCartDispatch({
+      type: "Update_Item",
+      Id: id,
+      op: op,
     });
   }
 
   const cart = {
-    Items: cartlist,
+    Items: shopingCartState,
     handleCountChane: handleCountChane,
     handleAddToCart: handleAddToCart,
   };
